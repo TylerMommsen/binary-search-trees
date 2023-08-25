367df028aed4266aa3e1822ec0eb8afe15f65c4f
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-08-25 13:19:05 +0300

    Added buildTree function to construct full BST tree

---
+++ index.js
@@ -0,0 +1,20 @@
+import Tree from "./tree.js";
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let BST = new Tree(array);
+// console.log(BST.root);
+
+prettyPrint(BST.root);
--- tree.js
+++ tree.js
@@ -1,5 +1,18 @@
+import Node from "./node.js";
+
 export default class Tree {
     constructor(arr) {
-        this.arr = arr;
+        this.root = this.buildTree(arr, 0, arr.length - 1);
+    }
+
+    buildTree(arr, start, end) {
+        if (start > end) return null;
+        let mid = parseInt((start + end) / 2);
+        let root = new Node(arr[mid]);
+
+        root.left = this.buildTree(arr, start, mid - 1);
+        root.right = this.buildTree(arr, mid + 1, end);
+
+        return root;
     }
 }
